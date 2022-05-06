74bb59091e72d4367638adc2360c39f686e0ba71
Peter Koomen <koomen@gmail.com>
2022-05-05 21:52:34 -0700

    Implement flag dependency demo

---
+++ docs/optimizely_flag_dependencies.js
@@ -0,0 +1,56 @@
+/**
+ * Returns an Optimizely client instance with an additional bound method: decideWithDependencies
+ */
+function getOptimizelyClient() {
+	var optimizelyClient = optimizelySdk.createInstance({
+		sdkKey: "LbmzK7viE2J2bP5ozmZR9"
+	});
+
+
+	optimizelyClient.decideWithDependencies = decideWithDependencies.bind(optimizelyClient);
+
+	return optimizelyClient;
+};
+
+/**
+ * Makes a flag decision that respects any dependencies specified in the '_depends' flag variable
+ * @param {Object} userContext  An OptimizelyUserContext object representing the user 
+ * @param {String} flagKey      The flag key 
+ * @param {Object} options      [Array of OptimizelyDecideOption enums]
+ */
+function decideWithDependencies(userContext, flagKey, options) {
+	var config = this.getOptimizelyConfig();
+	
+	// Check dependencies
+	if (config.featuresMap.hasOwnProperty(flagKey)) {
+
+		var flag = config.featuresMap[flagKey];
+		if (flag.variablesMap.hasOwnProperty("_depends")) {
+			var dependencies = flag.variablesMap["_depends"].value.split(/[, ]+/);
+			console.log(`Found dependencies for ${flagKey}: ${JSON.stringify(dependencies)}`);
+
+			// Evaluate each dependency
+			dependencies.forEach(dependency => {
+				console.log(`Checking dependency: ${dependency}`);
+
+				// If the dependency is not enabled, force the "off" variation to be returned
+				if (userContext.decide(dependency, { DISABLE_DECISION_EVENT : true }).enabled) {
+					console.log(`${flagKey} dependency ${dependency} is enabled`);
+				} else {
+					console.log(`${flagKey} dependency ${dependency} is disabled`);
+					console.log(userContext.setForcedDecision({flagKey:flagKey}, {variationKey:"off"}));
+				}
+			});
+
+		}
+	}
+
+	// Make the decision for flagKey
+	// If flagKey has any disabled dependencies, decide() will return "off"
+	var decision = userContext.decide(flagKey, options);
+
+	// Remove the forced decision rule
+	userContext.removeForcedDecision({flagKey: flagKey, ruleKey: null});
+
+	return decision;
+};
